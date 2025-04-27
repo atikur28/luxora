0410549a06c22a723227a9e17dabf4c59a277613
atikur28 <atikur45683@gmail.com>
2025-04-27 12:03:16 +0600

    Order placement completed

--- app/checkout/checkout-form.tsx
+++ app/checkout/checkout-form.tsx
@@ -43,6 +43,8 @@ import {
   AVAILABLE_PAYMENT_METHODS,
   DEFAULT_PAYMENT_METHOD,
 } from "@/lib/constants";
+import { createOrder } from "@/lib/actions/order.actions";
+import { toast } from "sonner";
 
 const shippingAddressDefaultValues =
   process.env.NODE_ENV === "development"
@@ -84,6 +86,7 @@ const CheckoutForm = () => {
     updateItem,
     removeItem,
     setDeliveryDateIndex,
+    clearCart,
   } = useCartStore();
   const isMounted = useIsMounted();
 
@@ -114,7 +117,26 @@ const CheckoutForm = () => {
     useState<boolean>(false);
 
   const handlePlaceOrder = async () => {
-    // TODO: place order
+    const res = await createOrder({
+      items,
+      shippingAddress,
+      expectedDeliveryDate: calculateFutureDate(
+        AVAILABLE_DELIVERY_DATES[deliveryDateIndex!].daysToDeliver
+      ),
+      deliveryDateIndex,
+      paymentMethod,
+      itemsPrice,
+      shippingPrice,
+      taxPrice,
+      totalPrice,
+    });
+    if (!res.success) {
+      toast.error(res.message);
+    } else {
+      toast.success(res.message);
+      clearCart();
+      router.push(`/checkout/${res.data?.orderId}`);
+    }
   };
   const handleSelectPaymentMethod = () => {
     setIsAddressSelected(true);
@@ -129,7 +151,7 @@ const CheckoutForm = () => {
         {!isAddressSelected && (
           <div className="border-b mb-4">
             <Button
-              className="rounded-full w-full"
+              className="rounded-full w-full cursor-pointer"
               onClick={handleSelectShippingAddress}
             >
               Ship to this address
@@ -143,7 +165,7 @@ const CheckoutForm = () => {
         {isAddressSelected && !isPaymentMethodSelected && (
           <div className=" mb-4">
             <Button
-              className="rounded-full w-full"
+              className="rounded-full w-full cursor-pointer"
               onClick={handleSelectPaymentMethod}
             >
               Use this payment method
@@ -158,7 +180,10 @@ const CheckoutForm = () => {
         )}
         {isPaymentMethodSelected && isAddressSelected && (
           <div>
-            <Button onClick={handlePlaceOrder} className="rounded-full w-full cursor-pointer">
+            <Button
+              onClick={handlePlaceOrder}
+              className="rounded-full w-full cursor-pointer"
+            >
               Place Your Order
             </Button>
             <p className="text-xs text-center py-2">
@@ -607,7 +632,7 @@ const CheckoutForm = () => {
                                   <Label
                                     className="flex flex-col items-start cursor-pointer"
                                     htmlFor={`address-${dd.name}`}
-                                  > 
+                                  >
                                     <div className="text-green-700 font-semibold">
                                       {
                                         formatDateTime(
@@ -654,7 +679,10 @@ const CheckoutForm = () => {
 
               <Card className="hidden md:block ">
                 <CardContent className="p-4 flex flex-col md:flex-row justify-between items-center gap-3">
-                  <Button onClick={handlePlaceOrder} className="rounded-full cursor-pointer">
+                  <Button
+                    onClick={handlePlaceOrder}
+                    className="rounded-full cursor-pointer"
+                  >
                     Place Your Order
                   </Button>
                   <div className="flex-1">
