8d8417fd724c483dfb32d6cab5d51b59b5d8ded9
atikur28 <atikur45683@gmail.com>
2025-05-02 22:42:15 +0600

    stripe-form.tsx file checking 4

--- app/checkout/[id]/stripe-form.tsx
+++ app/checkout/[id]/stripe-form.tsx
@@ -8,7 +8,6 @@ import { FormEvent, useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import ProductPrice from "@/components/shared/product/product-price";
-import { SERVER_URL } from "@/lib/constants";
 
 export default function StripeForm({
   priceInCents,
@@ -33,7 +32,7 @@ export default function StripeForm({
       .confirmPayment({
         elements,
         confirmParams: {
-          return_url: `${SERVER_URL}/checkout/${orderId}/stripe-payment-success`,
+          return_url: `https://luxora-psi.vercel.app/checkout/${orderId}/stripe-payment-success`,
         },
       })
       .then(({ error }) => {
