2dad9a61147c2c8d30fd10e9924ea56253d330a3
atikur28 <atikur45683@gmail.com>
2025-09-18 13:36:11 +0600

    Created website layout

---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "LuxOra";
+export const APP_SLOGAN =
+  process.env.NEXT_PUBLIC_APP_SLOGAN || "Elevate your lifestyle, for less.";
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  "LuxOra is your ultimate shopping destination — a powerful Amazon-inspired platform built with Next.js and MongoDB. Discover top deals, premium products, and seamless experiences all in one place.";
