9af9e4045a9c4c0d31575db12896d3ee905b1565
atikur28 <atikur45683@gmail.com>
2025-09-19 12:27:35 +0600

    Implemented add to cart

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,7 +1,14 @@
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "LuxOra";
+
 export const APP_SLOGAN =
   process.env.NEXT_PUBLIC_APP_SLOGAN || "Elevate your lifestyle, for less.";
+
 export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
   "LuxOra is your ultimate shopping destination — a powerful Amazon-inspired platform built with Next.js and MongoDB. Discover top deals, premium products, and seamless experiences all in one place.";
+
 export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9);
+
+export const FREE_SHIPPING_MIN_PRICE = Number(
+  process.env.FREE_SHIPPING_MIN_PRICE || 35
+);
